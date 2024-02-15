9e6feb35f97549790c5ab7b57766854cdb2c568b
Prathamesh Nale <141139818+Prathameshnale@users.noreply.github.com>
2024-02-15 17:31:31 +0530

    Update ProductService.java

--- cdac-proj/src/main/java/com/artgallery/cdacproj/service/ProductService.java
+++ cdac-proj/src/main/java/com/artgallery/cdacproj/service/ProductService.java
@@ -10,29 +10,10 @@ public interface ProductService {
 
 	List<Product> getProductsByCategory(String category);
 
-<<<<<<< HEAD
-
-
-=======
->>>>>>> bf7086c523a4e10284aadce6389851501672f0e5
 	List<Product> getProductByArtistName(String artistName);
 
 	void deleteById(int id);
 
 	void addnewProduct(Product p);
-<<<<<<< HEAD
-	
-	Product findById(long id);
-
-
 
 }
-
-
-
-=======
-
-	Product findById(Long productId);
-
-}
->>>>>>> bf7086c523a4e10284aadce6389851501672f0e5
