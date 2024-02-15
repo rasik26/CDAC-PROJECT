15e1e01b674aac05bd10fdfec711578594779783
Prathamesh Nale <141139818+Prathameshnale@users.noreply.github.com>
2024-02-15 15:09:22 +0530

    Update ProductServiceImpl.java

--- cdac-proj/src/main/java/com/artgallery/cdacproj/service/ProductServiceImpl.java
+++ cdac-proj/src/main/java/com/artgallery/cdacproj/service/ProductServiceImpl.java
@@ -10,8 +10,6 @@ import com.artgallery.cdacproj.model.Product;
 
 @Service
 public class ProductServiceImpl implements ProductService {
-	
-	
 	@Autowired
 	private ProductDao pdao;
 
@@ -27,10 +25,6 @@ public class ProductServiceImpl implements ProductService {
 	}
 
 	@Override
-<<<<<<< HEAD
-
-=======
->>>>>>> 2bc88c2efa2d8b1189fa12f5f4b0fd005190666c
 	public List<Product> getProductByArtistName(String artistName) {
 		return pdao.findByArtistName(artistName);
 	}
@@ -40,24 +34,9 @@ public class ProductServiceImpl implements ProductService {
 		pdao.deleteById((long) id);
 
 	}
-<<<<<<< HEAD
-	
-=======
 
->>>>>>> 2bc88c2efa2d8b1189fa12f5f4b0fd005190666c
 	public void addnewProduct(Product p) {
 		pdao.save(p);
 
 	}
-<<<<<<< HEAD
-
-	@Override
-	public Product findById(long id) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-
-=======
->>>>>>> 2bc88c2efa2d8b1189fa12f5f4b0fd005190666c
 }
